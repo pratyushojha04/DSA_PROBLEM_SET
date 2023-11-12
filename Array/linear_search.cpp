e6d8e29e4082817f298f6b064cd680d8cbd284c4
Pratyush ojha <82443757+pratyushojha04@users.noreply.github.com>
2023-11-12 10:04:23 +0530

    LL add question

---
+++ Array/linear_search.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 3;
+
+    int arr[3] = {1, 4, 2};
+    int key = 4;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            cout << "the element is at " << i;
+        }
+        else
+        {
+            cout << "the element did not found";
+        }
+    }
+
+    return 0;
+}
