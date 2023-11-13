34e99dead696e28697c5a386f7bedcfec59dcb93
Pratyush ojha <82443757+pratyushojha04@users.noreply.github.com>
2023-11-13 19:45:08 +0530

    Update ImplStackClass.cpp

--- stack/ImplStackClass.cpp
+++ stack/ImplStackClass.cpp
@@ -1,42 +1,88 @@
 #include <iostream>
-#include<stack>
+#include <stack>
 using namespace std;
-//stck can be impleented in two forms 
-//linkedlist and array
+// stck can be impleented in two forms
+// linkedlist and array
 
-class Stack{
-    //properties
-    public :
-    int *arr;
-    int top;
-    int size;
-    //behaviour
-    Stack(int size){
-        this->size=size;
-        arr=new int[size];
-        top=-1;
-
+class Stack
+{
+  // properties
+public:
+  int *arr;
+  int top;
+  int size;
+  // behaviour
+  Stack(int size)
+  {
+    this->size = size;
+    arr = new int[size];
+    top = -1;
+  }
+  void push(int element)
+  {
+    if (size - top > 1)
+    {
+      top++;
+      arr[top] = element;
     }
-    void push(int element){}
-    void pop(int element){}
+    else
+    {
+      cout << "Stack Overflow" << endl;
+    }
+  }
+  void pop()
+  {
+    if (top >= 0)
+    {
+      top--;
+    }
+    else
+    {
+      cout << "stack underflow" << endl;
+    }
+  }
 
-    void top(int element){}
+  void top() {}
 
-    int peak(int element){}
-    bool isEmpty(){}
+  int peek()
+  {
+    if (top >= 0 && top < size)
+      return arr[top];
+    else
+    {
+      cout << "stack is empty" << endl;
+      return -1;
+    }
+  }
 
+  bool isEmpty()
+  {
+    if (top == -1)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
 
+};
 
+int main()
+{
+  Stack st(5);
+  st.push(22);
+  st.push(43);
+  st.push(44);
+  cout << st.peek() << endl;
 
+  st.pop();
+  cout << st.peek() << endl;
+  st.pop();
+  cout << st.peek() << endl;
+  st.pop();
+  cout << st.peek() << endl;
 
-
-
-
-}
-
-
-
-int main() {
-    
   return 0;
 }
